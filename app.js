63b4359b5df533cf92e8531260a218d4911ed5e1
DEGGY_MUDI <iyinevb@outlook.com>
2017-10-25 11:56:48 +0100

    Refactor rain drops function

--- app.js
+++ app.js
@@ -1,45 +1,15 @@
 
 module.exports = 
 {
-	rainDrops: (InputNum) =>
-	{
-		let factorlist = [], i;
-        let mySet = new Set();
-        
-        for (i = 2; i <= InputNum; i++) 
-        {
-            while ((InputNum % i) === 0) {
-                mySet.add(i);
-                InputNum /= i;
-            }
-        }
-        factorlist = Array.from(mySet);
-        console.log(factorlist);
-        let emptyList = [];
-
-      for (let q = 0; q < factorlist.length; q++) 
-    {
-       if(factorlist[q]== 3)
-       {
-         
-        emptyList.push("pling");
-       }
-       if(factorlist[q] == 5)
-       {
-        emptyList.push("plang");
-       }
-       if(factorlist[q] == 7)
-       {
-        emptyList.push("plong");
-       }
-       else
-       {
-            factorlist[q].toString();
-       }
-} 
-
-result = emptyList.join("").toString();
-return result;
-	}
-
+	 rainDrops = (InputNum) =>
+  {
+    let emptyList = ""; 
+    if(InputNum % 3 === 0)emptyList += "pling";
+    if(InputNum % 5 === 0)emptyList += "plang";
+    if(InputNum % 7 === 0)emptyList += "plong";
+    if(emptyList.length === 0)emptyList += InputNum;
+      console.log(emptyList); // print to console. 
+      return emptyList; // for use in other programs
+  };
+  rainDrops(2);
 }
